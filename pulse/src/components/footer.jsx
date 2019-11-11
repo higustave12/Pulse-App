900e96ab5145043c0c47d5cf06c45914e59a0f80
Gad Ishimwe <coolshigad@gmail.com>
2019-11-11 18:11:06 +0200

    sign up page also as landing page

---
+++ pulse/src/components/footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "../css/sign-up.css";
+
+class Footer extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <footer>
+          <div>copy right © 2019 Pulse - Andela</div>
+        </footer>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Footer;
---
+++ pulse/src/components/header.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "../css/sign-up.css";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <header>
+          <h1>Pulse</h1>
+        </header>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Header;
